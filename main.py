e83b87db1ad8b002b81c6de0b4c735aab77d85fd
Jacob Rosen <jacobrosen819@gmail.com>
2023-02-02 04:21:27 +0000

    third row finished

--- main.py
+++ main.py
@@ -48,6 +48,7 @@ sudoku_board_square_3_int = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 square_number= 0
 sudoku_board_row_1_Numbers = []
 sudoku_board_row_2_Numbers = []
+sudoku_board_row_3_Numbers = []
 
 while len(sudoku_board_row_1_Numbers)!= 9:  
   num_place_1 =  random.choice(sudoku_board_row_1_int)
@@ -55,9 +56,9 @@ while len(sudoku_board_row_1_Numbers)!= 9:
   sudoku_board_row_1_int.remove(num_place_1)
   
   square_number += 1
-  if square_number <= 2:
+  if square_number <= 3:
     sudoku_board_square_1_int.remove(num_place_1)
-  elif square_number <= 5:
+  elif square_number <= 6:
     sudoku_board_square_2_int.remove(num_place_1)
   else:
     sudoku_board_square_3_int.remove(num_place_1)
@@ -79,6 +80,21 @@ while len(sudoku_board_row_2_Numbers)!= 9:
     num_place_2 =  random.choice(sudoku_board_square_3_int)
     sudoku_board_row_2_Numbers.append(num_place_2)
     sudoku_board_square_3_int.remove(num_place_2)
+
+while len(sudoku_board_row_3_Numbers)!= 9:
+  square_number += 1 
+  if square_number <= 21:
+    num_place_3 =  random.choice(sudoku_board_square_1_int)
+    sudoku_board_row_3_Numbers.append(num_place_3)
+    sudoku_board_square_1_int.remove(num_place_3)
+  elif square_number <= 24:
+    num_place_3 =  random.choice(sudoku_board_square_2_int)
+    sudoku_board_row_3_Numbers.append(num_place_3)
+    sudoku_board_square_2_int.remove(num_place_3) 
+  else:
+    num_place_3 =  random.choice(sudoku_board_square_3_int)
+    sudoku_board_row_3_Numbers.append(num_place_3)
+    sudoku_board_square_3_int.remove(num_place_3)
 
 #print(sudoku_board_row_1_Numbers[0]) #test
 
@@ -106,8 +122,27 @@ sudoku_board_row_2_spot_9 = sudoku_board_row_2_Numbers[8]
 
 sudoku_board_row_2 = [[sudoku_board_row_2_spot_1, sudoku_board_row_2_spot_2, sudoku_board_row_2_spot_3], [sudoku_board_row_2_spot_4, sudoku_board_row_2_spot_5, sudoku_board_row_2_spot_6], [sudoku_board_row_2_spot_7, sudoku_board_row_2_spot_8, sudoku_board_row_2_spot_9]]
 
+sudoku_board_row_3_spot_1 = sudoku_board_row_3_Numbers[0]
+sudoku_board_row_3_spot_2 = sudoku_board_row_3_Numbers[1]
+sudoku_board_row_3_spot_3 = sudoku_board_row_3_Numbers[2]
+sudoku_board_row_3_spot_4 = sudoku_board_row_3_Numbers[3]
+sudoku_board_row_3_spot_5 = sudoku_board_row_3_Numbers[4]
+sudoku_board_row_3_spot_6 = sudoku_board_row_3_Numbers[5]
+sudoku_board_row_3_spot_7 = sudoku_board_row_3_Numbers[6]
+sudoku_board_row_3_spot_8 = sudoku_board_row_3_Numbers[7]
+sudoku_board_row_3_spot_9 = sudoku_board_row_3_Numbers[8]
+
+sudoku_board_row_3 = [[sudoku_board_row_3_spot_1, sudoku_board_row_3_spot_2, sudoku_board_row_3_spot_3], [sudoku_board_row_3_spot_4, sudoku_board_row_3_spot_5, sudoku_board_row_3_spot_6], [sudoku_board_row_3_spot_7, sudoku_board_row_3_spot_8, sudoku_board_row_3_spot_9]]
+
+
 #print(sudoku_board_row_1_Numbers) #test
 print (sudoku_board_row_1)
 print (sudoku_board_row_2)
+print (sudoku_board_row_3)
 print(first_square_break)
+""""
+print (sudoku_board_square_1_int)
+print (sudoku_board_square_2_int)
+print (sudoku_board_square_3_int)
+"""
 #print(sudoku_board_row_1_spot_1) #test
